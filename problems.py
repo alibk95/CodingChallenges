fd21e7d5b25ccba83511cf7a484d48e375ff0550
alibk95 <abkarimi1995@gmail.com>
2020-09-19 16:59:19 +0200

    moving zeros to the end of the array problem added.

---
+++ problems.py
@@ -0,0 +1,18 @@
+class Solution:
+  def moveZeros(self, nums):
+    # input : [0, 0, 0, 2, 0, 1, 3, 4, 0, 0]
+    # output: [2, 1, 3, 4, 0, 0, 0, 0, 0, 0]
+    t = 0
+    for i in range(len(nums)):
+      if nums[i] != 0:
+        nums[t] = nums[i]
+        if i != 0:
+          nums[i] = 0
+        t += 1
+        print(nums)
+    pass
+
+nums = [0,0,0,0,5,0,1,0,0]
+Solution().moveZeros(nums)
+print(nums)
+
