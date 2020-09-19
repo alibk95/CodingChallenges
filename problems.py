75ab2809b6deeecc48a5fcb519afa56bd74d23d7
alibk95 <abkarimi1995@gmail.com>
2020-09-19 18:41:56 +0200

    problem kth largest number in the list added

--- problems.py
+++ problems.py
@@ -9,9 +9,29 @@ class Solution:
         if i != 0:
           nums[i] = 0
         t += 1
-    pass
+    return nums
 
+  def kth_largest_num(self, nums, k):
+    e = 0
+    l = []
+    for j in range(k):
+      t = 0
+      for i in range(len(nums)):
+        if nums[i] > e:
+          t = nums[i]
+
+        nums[i] = 0
+
+    return t,l
+
+# driver code: 1
 nums = [0,0,0,0,5,0,1,0,0]
-Solution().moveZeros(nums)
-print(nums)
+print(Solution().moveZeros(nums))
+
+# driver code: 2
+nums = [3, 5, 7, 8, 9, 2]
+k = 3
+# res: 6
+print(Solution().kth_largest_num(nums, k))
+
 
